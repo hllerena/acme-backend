952356332260dfddb55a41dd8f8a8b17f9cbd527
hllerena <henryll27@hotmail.com>
2021-07-07 11:18:16 -0500

    Version inicial

---
+++ src/main/java/com/acme/backend/model/entity/Employee.java
@@ -0,0 +1,16 @@
+package com.acme.backend.model.entity;
+
+import java.io.Serializable;
+
+
+
+public class Employee {
+	
+	private static final long serialVersionUID = ID;
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private String address;
+	
+}
